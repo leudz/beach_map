5b6c4a76d25aed7aa181785313d817a93f1c5d73
David Komer <david.komer@gmail.com>
2020-12-28 20:53:03 +0200

    impl Hash for ID

--- src/id/mod.rs
+++ src/id/mod.rs
@@ -1,3 +1,5 @@
+use std::hash::{Hash, Hasher};
+
 #[cfg(feature = "serde")]
 mod serde;
 
@@ -6,4 +8,11 @@ mod serde;
 pub struct ID<V> {
     pub(crate) index: usize,
     pub(crate) version: V,
+}
+
+impl <V: Hash> Hash for ID<V> {
+    fn hash<H: Hasher>(&self, state:&mut H) {
+        self.index.hash(state);
+        self.version.hash(state);
+    }
 }
